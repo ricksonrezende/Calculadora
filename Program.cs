1a273e21bb4b5108d1028258eed4ec5a47bcca80
ricksonrezende <ricksonrezende1@gmail.com>
2023-03-29 09:09:35 -0300

    Implementado método e código do Menu.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Multiplicacao();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("O que deseja fazer?");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Selecione uma opção");
+            short res = short.Parse(Console.ReadLine());
         }
 
         static void Soma()
