c93dccc0ced0dcc92067e14ebac63e2424ebb3f0
ricksonrezende <ricksonrezende1@gmail.com>
2023-03-29 00:30:08 -0300

    Implementado código de soma e encapsulado dentro de um método chamado Soma.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,23 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma()
+        {
             Console.Clear();
+
             Console.WriteLine("Primeiro valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(v1);
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine($"O resultado da soma é {resultado}");
         }
     }
 }
